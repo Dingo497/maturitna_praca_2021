dcff4f0bba4faadeb54944b0fdfd81f447f37636
Martin <mart-in@centrum.sk>
2021-02-20 18:12:33 +0100

    Pridanie mojho linku na github do footera a odstranenie profile z headera

--- inc/footer.php
+++ inc/footer.php
@@ -74,7 +74,7 @@
         <div class="row pt-5 mt-5 text-center">
           <div class="col-md-12">
             <p>
-            Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This website is made <i class="icon-heart" aria-hidden="true"></i> by <a href="" target="_blank" class="text-primary">Martin Banas</a>
+            Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This website is made <i class="icon-heart" aria-hidden="true"></i> by <a href="https://github.com/Dingo497" target="_blank" class="text-primary">Martin Banas</a>
             </p>
           </div>
           
--- inc/header.php
+++ inc/header.php
@@ -75,7 +75,6 @@
             <li><a href="about.php">About</a></li>
             <li><a href="shop.php">Shop</a></li>
             <li><a href="contact.php">Contact</a></li>
-            <li><a href="profile.php">Profile</a></li>
           </ul>
         </div>
       </nav>
