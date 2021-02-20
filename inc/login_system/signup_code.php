24dc3248eb8577293e05ec1b1e0f7508f7947cd8
Martin <mart-in@centrum.sk>
2021-02-20 21:23:08 +0100

    Vytvorenie validacii signup_code.php

--- inc/login_system/signup_code.php
+++ inc/login_system/signup_code.php
@@ -1,1 +1,43 @@
 <?php
+
+require_once '../config.php';
+include_once 'functions.php';
+
+if (isset($_POST['submit_register'])) {
+	
+	$name = $_POST['firstname'];
+	$lname = $_POST['lastname'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$checkbox = $_POST['Check1'];
+
+//if je prazdny string
+	if (emptyInputSignup($name, $lname, $email, $password, $checkbox) !== false) {
+		header("location: ../../sign_up.php?error=emptyinput");
+		exit(); //stopne tento script totalne
+	}
+
+//if je platne user id
+	if (invalidUid($email) !== false) {
+		header("location: ../../sign_up.php?error=invaliduid");
+		exit();
+	}
+
+//if je dobry email
+	if (invalidEmail($email) !== false) {
+		header("location: ../../sign_up.php?error=invalidemail");
+		exit();
+	}
+
+//if user uz existuje v databaze
+	if (uidExists($connect, $email) !== false) { //mozno conn len
+		header("location: ../../sign_up.php?error=usernametaken");
+		exit(); //stopne tento script totalne
+	}
+
+	createUser($connect, $name, $email, $name, $lname, $password);
+
+}else{
+	header("location: ../../sign_up.php");
+	exit();
+}
